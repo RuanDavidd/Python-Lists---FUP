8dc87d8488ccc499a77a04108a0ba13ba8d3d734
Ruan David <122119174+RuanDavidd@users.noreply.github.com>
2023-10-08 19:49:12 -0300

    Update lista05.py

--- Listas FUP/lista05.py
+++ Listas FUP/lista05.py
@@ -82,19 +82,3 @@ anos = num // 365
 semanas = num % 365 // 7
 dias = num % 365 % 7
 print (f"anos:{anos}, semanas: {semanas}, dias: {dias}")'''
-
-n1 = int(input("digite um numero:"))
-n2 = int(input("digite um numero:"))
-n3 = int(input("digite um numero:"))
-if n1 >= n2 >= n3:
-    print(n1, n2, n3)
-elif n1 >= n3 >= n2:
-    print(n1, n3, n2)
-elif n2 >= n1 >= n3:
-    print(n2, n1, n3)
-elif n2 >= n3 >= n1:
-    print(n2, n3, n1)
-elif n3 >= n1 >= n2:
-    print(n3, n1, n2)
-else:
-    print(n3, n2, n1)
