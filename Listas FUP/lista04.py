8d8e3ad7feb54cf864a4114fdded84a939f871a2
Ruan David <122119174+RuanDavidd@users.noreply.github.com>
2023-10-09 22:01:35 -0300

    Update lista04.py

--- Listas FUP/lista04.py
+++ Listas FUP/lista04.py
@@ -13,6 +13,7 @@ while c < 10:
         menor = num
     c += 1
 print(f"o maior é {maior} e o menor é {menor}")'''
+
 #faça um programa q recebe um numero e devolva em uma letra equivalente no alfabeto
 '''letras = ["a", "b", "c", "d", "e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 num = int(input("digite um numero:"))
@@ -21,3 +22,50 @@ if 1 <= num <= 26:
 else:
     print("digite um valor dentro de 26")'''
 
+# Questão 10
+'''n = int(input("digite um numero: "))
+imp = 1
+for cont in range(1, n+1):
+    print(f"{imp} é ímpar")
+    imp += 2'''
+
+# Questão 11
+'''cont = 0
+soma = 0
+par = 0
+while cont < 50:
+    par += 2
+    soma += par
+    cont += 1
+print(soma)'''
+
+# Questão 12
+'''n = int(input("digite um valor maior que zero: "))
+cont = 1
+fat = n
+while cont < n:
+    fat *= cont
+    cont += 1
+print(f"o fatorial de {n} é {fat}")'''
+
+# Questão 13
+'''n = int(input("digite um numero:"))
+cont = 1
+while cont <= n:
+    if (n % cont) == 0:
+        print(f"{cont} é divisor de {n}")
+    cont += 1'''
+
+# Questão 14
+'''n = int(input("digite um valor maior que zero:"))
+div = 0
+cont = 1
+while cont <= n:
+    if (n % cont) == 0:
+        div += 1
+    cont += 1
+if div == 2:
+    print(f"O número {n} é primo")
+else:
+    print(f"{n} não é primo")
+print(div)'''
